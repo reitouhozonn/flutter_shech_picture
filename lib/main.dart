fbc127855811fe025bd55b6fc5b2168a1b88c8ba
teionhozonn <26685301+reitouhozonn@users.noreply.github.com>
2023-09-20 19:53:29 +0900

    fix class pixabayImage

--- lib/main.dart
+++ lib/main.dart
@@ -27,15 +27,58 @@ class PixabayPage extends StatefulWidget {
   State<PixabayPage> createState() => _PixabayPageState();
 }
 
+class PixabayImage {
+  final String previewURL;
+  final int likes;
+  final String webformatURL;
+
+  PixabayImage(
+      {required this.previewURL,
+      required this.likes,
+      required this.webformatURL});
+
+  factory PixabayImage.fromMap(Map<String, dynamic> map) {
+    return PixabayImage(
+      previewURL: map['previewURL'],
+      likes: map['likes'],
+      webformatURL: map['webformatURL'],
+    );
+  }
+}
+
 class _PixabayPageState extends State<PixabayPage> {
-  List imageList = [];
+  List<PixabayImage> pixabayImages = [];
 
   Future<void> fetchImages(String text) async {
-    Response response = await Dio().get(
-        'https://pixabay.com/api/?key=39403222-fea081f96daf081db91214c01&q= $text&image_type=photo');
-
-    imageList = response.data['hits'];
+    final response = await Dio().get(
+      // 'https://pixabay.com/api/?key=39403222-fea081f96daf081db91214c01&q= $text&image_type=photo',
+      'https://pixabay.com/api/',
+      queryParameters: {
+        'key': '39403222-fea081f96daf081db91214c01',
+        'q': text,
+        'image_type': 'photo',
+        'per_page': '40',
+      },
+    );
+    final List hits = response.data['hits'];
+    pixabayImages = hits.map((e) => PixabayImage.fromMap(e)).toList();
     setState(() {});
+  }
+
+  Future<void> shareImage(String url) async {
+    final dir = await getTemporaryDirectory();
+
+    final response = await Dio().get(
+      url,
+      options: Options(
+        responseType: ResponseType.bytes,
+      ),
+    );
+
+    final imageFile = XFile('${dir.path} + ${response.data}');
+    print(imageFile);
+
+    await Share.shareXFiles([imageFile]);
   }
 
   @override
@@ -63,29 +106,18 @@ class _PixabayPageState extends State<PixabayPage> {
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
         ),
-        itemCount: imageList.length,
+        itemCount: pixabayImages.length,
         itemBuilder: (context, index) {
-          Map<String, dynamic> image = imageList[index];
+          final pixabayImage = pixabayImages[index];
           return InkWell(
             onTap: () async {
-              Directory dir = await getTemporaryDirectory();
-              print(dir.path);
-
-              Response response = await Dio().get(image['webformatURL'],
-                  options: Options(
-                    responseType: ResponseType.bytes,
-                  ));
-
-              XFile imageFile = await XFile('${dir.path} + ${response.data}');
-
-              // await Share.shareXFiles([imageFile.]);
-              // print('${imageFile}');
+              shareImage(pixabayImage.webformatURL);
             },
             child: Stack(
               fit: StackFit.expand,
               children: [
                 Image.network(
-                  image['previewURL'],
+                  pixabayImage.previewURL,
                   fit: BoxFit.cover,
                 ),
                 Align(
@@ -100,7 +132,7 @@ class _PixabayPageState extends State<PixabayPage> {
                           size: 14,
                         ),
                         Text(
-                          image['likes'].toString(),
+                          pixabayImage.likes.toString(),
                         ),
                       ],
                     ),
